f071983797879086cb8d8e62aeaab13ccd1a3b90
grantbergland <grantbergland@gmail.com>
2022-08-15 05:33:09 -0500

    commit tree bbf3c328765b6acfe92e8cfa011cc72245300b8b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1*num2
 
-// Write your code here
+let random = parseInt(Math.random()*10)
+
+{const num3 = 20
+const num4 = 5
+let mod= num3/num4}
+
+let max=Math.max(18,19,20)
